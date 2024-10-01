af488475b0370b32699f1e8a4441d08f7f9c9f4d
DragonStorm245 <129889068+DragonStorm245@users.noreply.github.com>
2024-10-01 10:50:42 -0400

    Adding code for door

--- XRI_project/Assets/Scripts/DoorFeatures.cs
+++ XRI_project/Assets/Scripts/DoorFeatures.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class DoorFeatures : MonoBehaviour
+public class DoorFeatures : CoreFeatures //Inheritence
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Door Configuration")]
+
+    [SerializeField]
+    private Transform doorPivot; //Controls pivot
+
+    [SerializeField]
+    private float maxAngle = 90.0f;
+
+    [SerializeField]
+    private bool reverseAngleDirection = false;
+
+    [SerializeField]
+    private float doorSpeed = 1.0f;
+
+    [SerializeField]
+    private bool open = false;
+
+    [SerializeField]
+    private bool MakeKinematicOnOpen = false;
+
+    [Header("Interactions Configuration")]
+
+    [SerializeField]
+    private XRSocketInteractor socketInteractor;
+
+    [SerializeField]
+    private XRSimpleInteractable simpleInteractable;
+
+    private void Start()
     {
-        
+        socketInteractor?.selectEntered.AddListener((s) => 
+        {
+            OpenDoor();
+            PlayOnStart();
+        });
+
+        simpleInteractable?.selectEntered.AddListener((s) =>
+        {
+            OpenDoor();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenDoor()
     {
-        
+        if (!open)
+        {
+            PlayOnStart();
+            open = true;
+            StartCoroutine(ProcessedMotion());
+        }
+    }
+
+    private IEnumerator ProcessedMotion()
+    {
+        //Constantly look to confirm door is open
+        while (open)
+        {
+            var angle = doorPivot.localEulerAngles.y < 100 ? doorPivot.localEulerAngles.y : doorPivot.localEulerAngles.y -360;
+
+            angle = reverseAngleDirection ? Mathf.Abs(angle) : angle;
+
+            if (angle <= maxAngle)
+            {
+                doorPivot?.Rotate(Vector3.up, doorSpeed * Time.deltaTime * (reverseAngleDirection ? -1 : 1));
+            }
+
+            else
+            {
+                //when done interacting, turn off rigidbody
+                open = false;
+                var featureRigidBody = GetComponent<Rigidbody>();
+                if (featureRigidBody != null && MakeKinematicOnOpen) featureRigidBody.isKinematic = true;
+
+            }
+
+            yield return null;
+        }
     }
 }
